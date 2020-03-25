cf7b2f24d5aaf0725e37d81b11e1c5657ae22d96
Murray Smith <murray@mismith.info>
2020-03-24 23:20:18 -0600

    replace chart lib and graphed data

---
+++ src/components/HorizontalBarChart.vue
@@ -0,0 +1,16 @@
+<script>
+import Vue from 'vue';
+import { HorizontalBar, mixins } from 'vue-chartjs';
+
+export default Vue.extend({
+  extends: HorizontalBar,
+  name: 'HorizontalBarChart',
+  mixins: [mixins.reactiveProp],
+  props: {
+    chartOptions: Object,
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.chartOptions);
+  },
+});
+</script>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,87 +1,53 @@
 <template>
-  <div class="Statistics d-flex flex-column flex">
-    <v-app-bar app dense class="flex-grow-0">
-      <v-spacer />
-      <v-toolbar-items class="align-center">
-        <label
-          for="toggle"
-          class="v-label theme--light mr-2"
-          style="cursor: pointer;"
-        >
-          Absolute
-        </label>
-        <v-switch
-          v-model="useParticipantRelativeValues"
-          id="toggle"
-          hide-details
-          class="align-center"
-        >
-          <template #label>
-            Relative
-            <v-tooltip bottom max-width="300">
-              <template #activator="{ on }">
-                <v-icon class="ml-2" v-on="on" @click.stop>mdi-help-circle</v-icon>
-              </template>
-
-              <p>
-                <strong>Absolute</strong> values are the raw numbers as entered by participants.
-              </p>
-              <p>
-                <strong>Relative</strong> values are averaged by the number of
-                participants within a group; this allows small groups to be
-                <strong>on even footing</strong> with big ones.
-              </p>
-            </v-tooltip>
-          </template>
-        </v-switch>
-      </v-toolbar-items>
-      <v-spacer />
-    </v-app-bar>
-
-    <div class="d-flex flex-column justify-space-between flex">
-      <GChart
-        type="BarChart"
-        :data="groupData"
-        :options="{ ...groupOptions, title: 'Overall' }"
-        class="my-4"
-        @ready="loaded.overall = true;"
-      />
-      <Loader class="ma-auto" v-if="!loaded.overall" />
-
-      <v-divider />
-      <GChart
-        type="AreaChart"
-        :data="aggregatedData"
-        :options="{ ...options, title: 'Daily Progression' }"
-        class="my-4"
-        @ready="loaded.progress = true;"
-      />
-      <Loader class="ma-auto" v-if="!loaded.progress" />
-
-      <v-divider />
-      <GChart
-        type="ColumnChart"
-        :data="data"
-        :options="{ ...options, title: 'Daily Contributions' }"
-        class="my-4"
-        @ready="loaded.aggregate = true;"
-      />
-      <Loader class="ma-auto" v-if="!loaded.aggregate" />
-    </div>
+  <div class="Statistics d-flex flex-column flex-grow">
+    <BarChart
+      :chart-data="groupDataPerParticipant"
+      :chart-options="chartOptions"
+      class="flex my-4"
+    />
+    <BarChart
+      :chart-data="groupData"
+      :chart-options="chartOptions"
+      class="flex my-4"
+    />
+    <HorizontalBarChart
+      :chart-data="participantData"
+      :chart-options="{
+        ...chartOptions,
+        elements: {
+          rectangle: {
+            backgroundColor: ({ dataIndex }) => get(participants, `${dataIndex}.$group.color`),
+          },
+        },
+      }"
+      class="flex my-4"
+    />
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
-import { GChart } from 'vue-google-charts';
+import get from 'lodash.get';
 import groupBy from 'lodash.groupby';
-import { DateTime } from 'luxon';
 import { idKey } from '@/plugins/firebase';
-import Loader from '@/components/Loader.vue';
+import BarChart from '@/components/BarChart.vue';
+import HorizontalBarChart from '@/components/HorizontalBarChart.vue';
 
 function getParticipantRelativeValue(value, groupParticipants, id) {
   const numGroupParticipants = (groupParticipants[id] || []).length;
   return numGroupParticipants ? value / numGroupParticipants : 0;
+}
+function gatherData(allEntries, items, key, transformValue = (v) => v) {
+  return {
+    labels: items.map(({ name }) => name),
+    datasets: [{
+      data: items.map((item) => {
+        const entries = allEntries.filter((entry) => entry[key] === item[idKey]);
+        const sum = entries.reduce((acc, { value }) => acc + value, 0);
+        return transformValue(sum, item);
+      }),
+    }],
+  };
 }
 
 export default Vue.extend({
@@ -94,125 +60,68 @@ export default Vue.extend({
     entries: Array,
     compliments: Array,
   },
-  localStorage: {
-    useParticipantRelativeValues: {
-      type: Boolean,
-      default: true,
-    },
-  },
   data() {
     return {
       idKey,
-      loaded: {
-        overall: false,
-        progress: false,
-        aggregate: false,
+
+      chartOptions: {
+        legend: false,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            ticks: {
+              min: 0,
+              beginAtZero: true,
+            },
+          }],
+          yAxes: [{
+            ticks: {
+              min: 0,
+              beginAtZero: true,
+            },
+          }],
+        },
+        elements: {
+          rectangle: {
+            backgroundColor: ({ dataIndex }) => get(this.groups, `${dataIndex}.color`),
+          },
+        },
       },
     };
   },
   computed: {
-    groupParticipants() {
-      return groupBy(this.participants, ({ groupId }) => groupId);
-    },
-
-    options() {
-      const options = {
-        isStacked: true,
-        series: this.groups.map(({ color }) => ({ color })),
-        legend: this.$vuetify.breakpoint.smAndDown ? 'top' : 'right',
-        height: 300,
-      };
-      // console.log(options);
-      return options;
-    },
-    data() {
-      const data = [
-        [
-          'Date',
-          ...this.groups.map(({ name }) => name),
-        ],
-        ...Object.entries(groupBy(
-          this.entries,
-          ({ createdAt }) => DateTime.fromJSDate(createdAt.toDate()).toISODate(),
-        ))
-          .map(([label, entries]) => [
-            label,
-            ...this.groups.map(
-              ({ [idKey]: id }) => {
-                const sum = entries
-                  .filter(({ groupId }) => groupId === id)
-                  .reduce((acc, { value }) => acc + value, 0);
-                if (this.useParticipantRelativeValues) {
-                  return getParticipantRelativeValue(sum, this.groupParticipants, id);
-                }
-                return sum;
-              },
-            ),
-          ]),
-      ];
-      // console.log(data);
-      return data;
-    },
-    aggregatedData() {
-      const aggregatedData = [];
-      this.data.forEach((row, i) => {
-        if (i > 1) {
-          const aggregatedRow = [];
-          row.forEach((col, j) => {
-            if (j > 0) {
-              aggregatedRow.push(col + aggregatedData[i - 1][j]);
-              return;
-            }
-            aggregatedRow.push(col);
-          });
-          aggregatedData.push(aggregatedRow);
-          return;
-        }
-        aggregatedData.push(row);
-      });
-      // console.log(aggregatedData);
-      return aggregatedData;
-    },
-
-    groupOptions() {
-      const groupOptions = {
-        legend: 'none',
-        hAxis: {
-          minValue: 0,
-        },
-        height: 300,
-      };
-      // console.log(groupOptions);
-      return groupOptions;
-    },
     groupData() {
-      const groupData = [
-        [
-          'Group',
-          'Total',
-          { role: 'style' },
-        ],
-        ...Object.entries(groupBy(this.entries, ({ $group }) => $group.name))
-          .map(([label, entries]) => {
-            const group = entries[0].$group;
-            let sum = entries.reduce((acc, { value }) => acc + value, 0);
-            if (this.useParticipantRelativeValues) {
-              sum = getParticipantRelativeValue(sum, this.groupParticipants, group[idKey]);
-            }
-            return [
-              label,
-              sum,
-              group.color,
-            ];
-          }),
-      ];
+      const groupData = gatherData(this.entries, this.groups, 'groupId');
       // console.log(groupData);
       return groupData;
     },
+
+    groupParticipants() {
+      return groupBy(this.participants, ({ groupId }) => groupId);
+    },
+    groupDataPerParticipant() {
+      const groupDataPerParticipant = gatherData(
+        this.entries,
+        this.groups,
+        'groupId',
+        (sum, group) => getParticipantRelativeValue(sum, this.groupParticipants, group[idKey]),
+      );
+      // console.log(groupDataPerParticipant);
+      return groupDataPerParticipant;
+    },
+
+    participantData() {
+      const participantData = gatherData(this.entries, this.participants, 'participantId');
+      // console.log(participantData);
+      return participantData;
+    },
+  },
+  methods: {
+    get,
   },
   components: {
-    Loader,
-    GChart,
+    BarChart,
+    HorizontalBarChart,
   },
 });
 </script>
