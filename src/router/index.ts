d0bafb925fea16ef2e4a7b4b70f5f0eba55bdf99
Murray Smith <murray@mismith.info>
2020-04-10 14:27:28 -0600

    add page titles

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,26 +7,39 @@ import Statistics from '@/views/Statistics.vue';
 
 Vue.use(VueRouter);
 
+const title = 'challenge';
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home,
+    meta: {
+      title,
+    },
   },
   {
     path: '/activity',
     name: 'activity',
     component: Activity,
+    meta: {
+      title: `Activity • ${title}`,
+    },
   },
   {
     path: '/new',
     name: 'new',
     component: NewEntry,
+    meta: {
+      title: `New • ${title}`,
+    },
   },
   {
     path: '/statistics',
     name: 'statistics',
     component: Statistics,
+    meta: {
+      title: `Stats • ${title}`,
+    },
   },
 ];
 
@@ -34,6 +47,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  if (to?.meta?.title) {
+    document.title = to.meta.title;
+  }
+  return next();
 });
 
 export default router;
