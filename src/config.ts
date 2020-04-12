c7b08eee6302be699d87be2b44131148c0fde932
Murray Smith <murray@mismith.info>
2020-04-12 15:43:59 -0600

    fix accidentally release config override

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 import $package from '@/../package.json';
 
 export { $package };
-export const isDebugging = window.location.hostname === 'localhost' && 0;
+export const isDebugging = window.location.hostname === 'localhost';
